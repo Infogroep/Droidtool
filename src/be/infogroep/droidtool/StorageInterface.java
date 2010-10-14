2b02dfd25c697ee9fe2d20dce86bf25ef0881d36
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2010-10-14 17:26:04 +0200

    changed Storage Interface

--- src/be/infogroep/droidtool/StorageInterface.java
+++ src/be/infogroep/droidtool/StorageInterface.java
@@ -1,7 +1,9 @@
 package be.infogroep.droidtool;
 
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -27,16 +29,34 @@ public class StorageInterface {
 	}
 	
 	public String Get(Context ctx) throws FileNotFoundException {
-				
-		FileInputStream fin = ctx.openFileInput(FILENAME); //openFileOutput underlined red
-		byte[] token = null;
-        try {
-          fin.read(token);
-          fin.close();
-        } catch (IOException e) {
-          //Log.e("Controller", e.getMessage() + e.getLocalizedMessage() + e.getCause());
-        }
-		return token.toString();
 
+		String line = null;
+		
+		try {
+			InputStream fin = ctx.openFileInput(FILENAME); 
+			// if file the available for reading
+
+			// prepare the file for reading
+			InputStreamReader input = new InputStreamReader(fin);
+			BufferedReader buffreader = new BufferedReader(input);
+
+			
+
+			// read every line of the file into the line-variable, on line at the time
+			//while (( line = buffreader.readLine())) {
+				// do something with the settings from the file
+			//}
+			
+			line = buffreader.readLine();
+			// close the file again
+			fin.close();
+		} catch (java.io.FileNotFoundException e) {
+			// do something if the myfilename.txt does not exits
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return line;
 	}
 }
